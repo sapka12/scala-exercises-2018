38c4ff454287a850b2ee3cef9cb50f81dd8f0264
Arnold Farkas <farkasarnold1981@gmail.com>
2018-07-03 16:55:30 +0200

    Either impl

--- src/main/scala/errorhandling/Either.scala
+++ src/main/scala/errorhandling/Either.scala
@@ -1,16 +1,29 @@
 package errorhandling
 
+
 import scala.{List => _, Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 import datastructures.{List, Cons, Nil}
 
 sealed trait Either[+E, +A] {
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f(a))
+    case ee: Left[E] => ee
+  }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(_) => this
+    case _ => b
+  }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for {
+    aa <- this
+    bb <- b
+  } yield f(aa, bb)
 }
 
 case class Left[+E](get: E) extends Either[E, Nothing]
@@ -36,8 +49,11 @@ object Either {
       case e: Exception => Left(e)
     }
 
-  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    List.foldRight[A, Either[E, List[B]]](es, Right(Nil))(
+      (a, eitherListB) => f(a).map2(eitherListB)(Cons(_, _))
+    )
 
-  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = ???
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = traverse(es)(x => x)
 
 }
