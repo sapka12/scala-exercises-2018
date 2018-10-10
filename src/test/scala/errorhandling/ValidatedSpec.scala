a849356fce5258a28aa1be8b1e084a60e7d6d549
Arnold Farkas <farkasarnold1981@gmail.com>
2018-10-10 09:42:20 +0200

    errorhandling exercises

--- src/test/scala/errorhandling/ValidatedSpec.scala
+++ src/test/scala/errorhandling/ValidatedSpec.scala
@@ -1,8 +1,8 @@
 package errorhandling.validated
 
-import cats.data.Validated._
 import cats.data._
 import cats.implicits._
+import cats.data.Validated._
 import form.{Female, Male, Sex, User}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -16,25 +16,10 @@ class ValidatedSpec extends FlatSpec with Matchers {
   private val AGE_ERROR_MSG = "age is not valid"
   private val SEX_ERROR_MSG = "should be female"
 
-  private def eitherName(name: String) = if(optimalLength(name)) Right(name) else Left(NAME_ERROR_MSG)
-  private def eitherAge(age: Int) = if (adult(age)) Right(age) else Left(AGE_ERROR_MSG)
-  private def eitherSex(s: Option[Sex]) = s match {
-    case Some(sex) if female(sex) => Right(sex)
-    case _ => Left(SEX_ERROR_MSG)
-  }
-
-
   behavior of "option"
 
   it should "do a simple validation" in {
-    def createAdultFemaleUser(name: String, age: Int, sex: Option[Sex] = None): Option[User] = for {
-      n <- Some(name)
-      a <- Some(age)
-      s <- sex
-      if optimalLength(n)
-      if adult(a)
-      if female(s)
-    } yield User(n, a, sex)
+    def createAdultFemaleUser(name: String, age: Int, sex: Option[Sex] = None): Option[User] = ???
 
     createAdultFemaleUser("John", 10) shouldBe None
     createAdultFemaleUser("John Doe", 10) shouldBe None
@@ -46,11 +31,7 @@ class ValidatedSpec extends FlatSpec with Matchers {
 
   it should "do validation with error message" in {
 
-    def createAdultFemaleUser(name: String, age: Int, sex: Option[Sex] = None): Either[String, User] = for {
-      n <- eitherName(name)
-      a <- eitherAge(age)
-      s <- eitherSex(sex)
-    } yield User(n, a, Some(s))
+    def createAdultFemaleUser(name: String, age: Int, sex: Option[Sex] = None): Either[String, User] = ???
 
     createAdultFemaleUser("John", 10) shouldBe Left(NAME_ERROR_MSG)
     createAdultFemaleUser("John Doe", 10) shouldBe Left(AGE_ERROR_MSG)
@@ -63,16 +44,14 @@ class ValidatedSpec extends FlatSpec with Matchers {
   behavior of "validated"
 
   it should "do validation with error message(s)" in {
-    def createAdultFemaleUser(name: String, age: Int,  sex: Option[Sex] = None): Validated[NonEmptyList[String], User] = {
-      val n = if(optimalLength(name)) name.validNel else NAME_ERROR_MSG.invalidNel
-      val a = if (adult(age)) age.validNel else AGE_ERROR_MSG.invalidNel
-      val s = sex match {
-        case Some(_sex) if female(_sex) => Some(_sex).validNel
-        case _ => SEX_ERROR_MSG.invalidNel
-      }
 
-      (n, a, s) mapN User
-    }
+    def validName(name: String): Validated[NonEmptyList[String], String] = if(optimalLength(name)) name.validNel else NAME_ERROR_MSG.invalidNel
+    def validAge(age: Int): Validated[NonEmptyList[String], Int] = ???
+    def validSex(sex: Option[Sex]): Validated[NonEmptyList[String], Some[Sex]] = ???
+
+    def createAdultFemaleUser(name: String, age: Int,  sex: Option[Sex] = None): Validated[NonEmptyList[String], User] =
+      (validName(name), validAge(age), validSex(sex)).mapN(User)
+
 
     createAdultFemaleUser("John", 10) shouldBe
       Invalid(NonEmptyList.of(NAME_ERROR_MSG, AGE_ERROR_MSG, SEX_ERROR_MSG))
